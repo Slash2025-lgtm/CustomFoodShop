990e6c6e80ce5b5bfb7a5ca3c58dee90c552f79c
Student <bwilliams1@my.yearupunited.org>
2025-11-12 12:50:53 -0500

    Receipt + Drink Class Added + Pizza Class = Food

---
+++ src/main/java/com/pluralsight/ordering/Drink.java
@@ -0,0 +1,12 @@
+package com.pluralsight.ordering;
+
+import java.util.ArrayList;
+
+public class Drink extends Order {
+    public Drink() {}
+
+    @Override
+    public void addDrinkToOrder(ArrayList<String> orderInfo) {
+        super.addDrinkToOrder(orderInfo);
+    }
+}
